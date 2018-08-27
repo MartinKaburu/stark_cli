d384d51ceab0a074b0fa6a8f1ea29f433371a3c0
Mirriam <mirrmaina@gmail.com>
2018-08-27 14:21:50 +0300

    [Chore]: Edited user exists code

--- users.py
+++ users.py
@@ -5,38 +5,22 @@ cur = conn.cursor()
 
 
 class User(object):
-    def __init__(self, username, password):
-        ''' Initialize user '''
-        self.username = input("Username:")
-        self.password = input("Password:")
-
-    def user_exists(self):
-        """ Check if a user exists in the db """
-        conn = open_connection()
-        cur = conn.cursor()
-        cur.execute("SELECT * from users WHERE username='{}'".format(self.username))
-        user = cur.fetchone()
-        cur.close()
-        conn.commit()
-        return user
-
     def signup(self, role, username, password):
         """Signup the user"""
-        role = self.role()
-        user_exists = self.user_exists()
+        cur = conn.cursor()
+        cur.execute("SELECT * from users WHERE username='{}'".format(username))
+        user_exists = cur.fetchone()
         if not user_exists:
             print("that user already exists")
-        username = self.username
-        password = self.password
         cur.execute('insert into users(username,password, role) values({},{},{})'.format(username,password,role))
+        print("Successfully registered")
         
 
     def login(self, username, password): 
         """Login the user"""   
-        user_exists = self.user_exists()
+        cur = conn.cursor()
+        cur.execute("SELECT * from users WHERE username='{}'".format(username))
+        user_exists = cur.fetchone()
         if user_exists:
             print ("you have successfully logged in")
         print("wrong username or password")
-
-if __name__ == '__main__':
-    
