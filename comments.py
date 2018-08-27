26f8f9abc75d42bc06b3dd59aadbf7c6571409ea
WaitheraMbugua <waitheralmbugua@gmail.com>
2018-08-27 14:09:36 +0300

    [Feature

--- comments.py
+++ comments.py
@@ -3,8 +3,7 @@ import psycopg2
 class Comments(object):
 	def __init__(self,message, time_stamp, author, id, role):
 		self.id=id
-		self.message= message
-		self.time_stamp=time_stamp
+		self.message=message
 		self.author=author
 		self.role=role
 
@@ -23,11 +22,11 @@ class Comments(object):
         return False		
 		
 
-	def post_comment(self, role):
+	def post_comment(self, user_id, ):
 
-         comment = comment.query.filter_by(author=author).first()
-         if author ==
-		cur.execute('INSERT INTO * comments {'id','message', 'time_stamp', 'author'} VALUE (('%s', '%s', '%s', '%s')(self.id, self.message, self.time_stamp, self.author)));
+         comment = comment.query.filter_by(user_id=user_id).first()
+         if  ==
+		cur.execute('INSERT INTO * comments {'id','message', 'time_stamp', 'user_id'} VALUE (('%s', '%s', '%s', '%s')(self.id, self.message, self.time_stamp, self.user_id)));
 		(cursor)
 		conn.commit()
 
@@ -35,14 +34,24 @@ class Comments(object):
 	
 
 	
-	def edit_comment(self, id, role):
-		cur.execute("UPDATE comments SET message = %s, time_stamp=%s, author=%s WHERE id = %s)VALUE (("%s", '%s', '%s', '%s'  )(self.id, self.message, self.time_stamp, self.author)); 
-		conn.commit()
+	def edit_comment(self, input = 'Give me your user_id', ):
+		if role='1':
+
+		  cur.execute("UPDATE comments SET message = %s, user_id=%s WHERE id = %s)VALUE (("%s", '%s', '%s', '%s'  )(self.id, self.message, self.time_stamp, self.author)); 
+		  conn.commit()
+
+	    else:
+	     cur.execute("UPDATE comments SET message = %s, user_id=%s WHERE id = %s)VALUE (("%s", '%s', '%s', '%s'  )(self.id, self.message, self.time_stamp, self.author)); 
+		  conn.commit()
+		 
+
+
 		
 
 
 	def delete_comment(self,id, role):
-		if role==2, 3:
+
+		if role=='2'or'3':
 
     	comment = comments.query.filter_by(id=id).first()
         comment.delete()
