0c8b83c9a2d1f44345f2a60a3c712ff88edbe58a
Martin Kaburu <martinkaburu.m@gmail.com>
2018-09-06 16:55:31 +0300

    [ft] Implemented all features

--- comments.py
+++ comments.py
@@ -6,60 +6,101 @@ from users import  User
 auth = User()
 
 CONNECTION = psycopg.connect(\
-    dbname='starks', \
-    user='postgres', \
-    host='localhost', \
-    password='kaburu@andela'\
-    )
+dbname='starks', \
+user='postgres', \
+host='localhost', \
+password='kaburu@andela'\
+)
 
-class Comments(object):
-	def __init__(self):
-		'''Instantiate class
-		'''
-
-		self.cursor = CONNECTION.cursor()
-
-
-	def post_comment(self, comment):
-		'''Post a comment
-		'''
-
-		try:
-			ret, username = auth.auth()
-		except:
-			return "[+] Please log in to continue"
-		sql = 'SELECT * FROM users WHERE username=%s;'
-		self.cursor.execute(sql, ([username]))
-		user = self.cursor.fetchall()
-		user_id = user[0][0]
-		role = user[0][3]
-		sql = 'INSERT INTO comments(content, user_id) VALUES(%s, %s);'
-		self.cursor.execute(sql, (comment, user_id))
-		CONNECTION.commit()
-		return "[+] Comment posted successfully"
-
-	def all(self):
-		'''Get all comments
-		'''
-		sql = 'SELECT * FROM comments;'
-		self.cursor.execute(sql)
-		comments = self.cursor.fetchall()
-		display = []
-		for comment in comments:
-			comm = {
-				"id": comment[0],
-				"user_id": comment[1],
-				"content": comment[2]
-			}
-			display.append(comm)
-		return display
+class Comments():
+    '''Define comment methods
+    '''
+    def __init__(self):
+        '''Instantiate class
+        '''
+        self.cursor = CONNECTION.cursor()
 
 
-	def edit(self):
-		'''Edit a comment
-		'''
+    def check_auth(self):
+        '''check authorization
+        '''
+        try:
+            ret, username = auth.auth()
+        except:
+            return "[+] Please log in to continue"
+        sql = 'SELECT * FROM users WHERE username=%s;'
+        self.cursor.execute(sql, ([username]))
+        user = self.cursor.fetchall()
+        user_id = user[0][0]
+        role = user[0][3]
+        return user_id, role
+
+    def post_comment(self, comment):
+        '''Post a comment
+        '''
+        try:
+            user_id, role = self.check_auth()
+        except:
+            return self.check_auth()
+        sql = 'INSERT INTO comments(content, user_id) VALUES(%s, %s);'
+        self.cursor.execute(sql, (comment, user_id))
+        CONNECTION.commit()
+        return "[+] Comment posted successfully"
+
+    def all(self):
+        '''Get all comments
+        '''
+        sql = 'SELECT * FROM comments;'
+        self.cursor.execute(sql)
+        comments = self.cursor.fetchall()
+        display = []
+        for comment in comments:
+            comm = {
+            "id": comment[0],
+            "user_id": comment[1],
+            "content": comment[2]
+            }
+            display.append(comm)
+        return display
 
 
-	def delete(self):
-		'''Delete a comment
-		'''
+    def edit(self, update, id):
+        '''Edit a comment
+        '''
+        try:
+            user_id, role = self.check_auth()
+        except:
+            return "[+] Please log in to continue"
+        if role == 1 or role == 2:
+            sql = 'SELECT * FROM comments WHERE id=%s AND user_id=%s;'
+            self.cursor.execute(sql, (id, user_id))
+            comment = self.cursor.fetchone()
+            if comment:
+                sql = 'UPDATE comments SET content=%s WHERE id=%s AND user_id=%s;'
+                self.cursor.execute(sql, (update, id, user_id))
+                CONNECTION.commit()
+                return "[+] Comment updated successfully"
+            return "[+] You can only edit your own comments"
+        elif role == 3:
+            sql = 'UPDATE comments SET content=%s WHERE id=%s;'
+            self.cursor.execute(sql, (update, id))
+            CONNECTION.commit()
+            return "[+] Admin comment updated"
+        return "[+] Invalid role"
+
+
+
+
+    def delete(self, id):
+        '''Delete a comment
+        '''
+        try:
+            user_id, role = self.check_auth()
+        except:
+            return "[+] Please log in to continue"
+        if role == 3 or role == 2:
+            sql = 'DELETE FROM comments WHERE id=%s;'
+            self.cursor.execute(sql, ([id]))
+            CONNECTION.commit()
+            return "[+] Comment deleted successfully"
+        return "[+] Ooops your role doesn't authorize that"
